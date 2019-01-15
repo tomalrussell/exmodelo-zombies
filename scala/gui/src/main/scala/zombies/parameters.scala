d958460c1c001bc4bb793ee64302679b9a4ba995
Mathieu <mathieu.leclaire@iscpif.fr>
2019-01-15 12:53:09 +0100

    Add Options controller

--- scala/gui/src/main/scala/zombies/parameters.scala
+++ scala/gui/src/main/scala/zombies/parameters.scala
@@ -1,11 +1,14 @@
 package zombies
 
+import zombies.controls.{Mecanism}
+
 object parameters {
 
   trait ParameterType
 
   case class Doubles(min: Double, max: Double, step: Double, default: Double) extends ParameterType
   case class Ints(min: Int, max: Int, step: Double, default: Int) extends ParameterType
+  case class Options(mecanisms: Mecanism*) extends ParameterType
   case class Booleans(default: Boolean) extends ParameterType
 
   case class Parameter(name: String, parameterType: ParameterType)
@@ -25,5 +28,6 @@ object parameters {
     Parameter("zombieMaxRotation", Doubles(0.0, 180.0, 1.0, 45.0)),
     Parameter("# zombies", Ints(0, 1500, 1, 4)),
     Parameter("rotationGranularity", Ints(0, 10, 1, 5)),
+    Parameter("My mecanismes", Options("M1", "M2", "M3"))
   )
 }
