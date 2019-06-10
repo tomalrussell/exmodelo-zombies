6d85144d3efc464c8758d3f91ee54c171fad25b2
helenea <helenea@users.noreply.github.com>
2019-06-10 22:49:07 +0200

    [ODE] Plot parcimony

--- scala/ode/src/main/scala/zombies/ode/RFiles/parcimony_analysis.R
+++ scala/ode/src/main/scala/zombies/ode/RFiles/parcimony_analysis.R
@@ -2,6 +2,7 @@ library(deSolve)
 library(tidyverse)
 
 data <- read_csv("parcimonie_population20000.csv")
+save(data, file = "calib_parcimony.RData")
 
 pareto_front <- data %>%
     ggplot(aes(x = parcimony, y = fitness)) +
@@ -9,3 +10,5 @@ pareto_front <- data %>%
     geom_line() +
     theme_bw()
 pareto_front
+
+ggsave("plot_parcimony.png", pareto_front)
