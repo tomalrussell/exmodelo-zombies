e87a11ee301ff4c73a8febd2054d90590069c7cc
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-06-26 14:49:35 +0200

    spatialsens:results

--- openmole/spatialsens/analysis.R
+++ openmole/spatialsens/analysis.R
@@ -7,10 +7,13 @@ setwd(paste0(Sys.getenv('CS_HOME'),'/OpenMole/zombies/openmole/spatialsens'))
 #res1 <- as.tbl(read.csv('exploration/20190330_174556_LHS_SPATIALSENS_REPLICATIONS_GRID.csv'))
 #res2 <- as.tbl(read.csv('exploration/20190408_163218_LHS_SPATIALSENS_REPLICATIONS_GRID.csv'))
 #res3 <- as.tbl(read.csv('exploration/20190411_090002_LHS_SPATIALSENS_REPLICATIONS_GRID.csv'))
-res <- as.tbl(read.csv('exploration/20190412_152247_LHS_SPATIALSENS_REPLICATIONS_GRID.csv'))
+#res <- as.tbl(read.csv('exploration/20190412_152247_LHS_SPATIALSENS_REPLICATIONS_GRID.csv'))
+res <- as.tbl(read.csv('exploration/20190624_225411_LHS_SPATIALSENS_REPLICATIONS_GRID.csv'))
+
 #res=rbind(res1,res2,res3,res4)
 #resdir='results/20190330-20190411_LHS_SPATIALSENS_REPLICATIONS_GRID';dir.create(resdir)
-resdir='results/20190412_LHS_SPATIALSENS_REPLICATIONS_GRID';dir.create(resdir)
+#resdir='results/20190412_LHS_SPATIALSENS_REPLICATIONS_GRID';dir.create(resdir)
+resdir='results/20190624_225411_LHS_SPATIALSENS_REPLICATIONS_GRID';dir.create(resdir)
 
 
 finalTime = 50
@@ -23,6 +26,7 @@ sres = res %>% group_by(generatorType,blocksMaxSize,blocksMinSize,blocksNumber,e
 indics = c(paste0("humansDynamic",0:finalTime),paste0("zombiesDynamic",0:finalTime))
 indic1=paste0("humansDynamic",0:finalTime)
 indic2=paste0("zombiesDynamic",0:finalTime)
+indic=paste0("zombifiedDynamic",0:finalTime)
 
 params=c("generatorType","blocksMaxSize","blocksMinSize","blocksNumber",
          "expMixtureCenters","expMixtureRadius","expMixtureThreshold",
@@ -34,13 +38,18 @@ params=c("generatorType","blocksMaxSize","blocksMinSize","blocksNumber",
 
 reference1 = colMeans(res[res$generatorType=="jaude",indic1])
 reference2 = colMeans(res[res$generatorType=="jaude",indic2])
+reference = colMeans(res[res$generatorType=="jaude",indic])
 
 #sample = sample(1:nrow(res),10000)
 sample = 1:nrow(res)
 
-tres = melt(res[sample,c(indic1,params)],id.vars=params,measure.vars = indic1,variable.name = "var")
+tres = melt(res[sample,c(indic,params)],id.vars=params,measure.vars = indic,variable.name = "var")
 tres$time = c(matrix(rep(0:finalTime,length(sample)),ncol=finalTime+1,byrow = T))
-tres$indic = rep("humans",nrow(tres))
+tres$indic = rep("zombified",nrow(tres))
+
+tres1 = melt(res[sample,c(indic1,params)],id.vars=params,measure.vars = indic1,variable.name = "var")
+tres1$time = c(matrix(rep(0:finalTime,length(sample)),ncol=finalTime+1,byrow = T))
+tres1$indic = rep("humans",nrow(tres))
   
 tres2 = melt(res[sample,c(indic2,params)],id.vars=params,measure.vars = indic2,variable.name = "var")
 tres2$time = c(matrix(rep(0:finalTime,length(sample)),ncol=finalTime+1,byrow = T))
@@ -63,6 +72,13 @@ stres = tres %>%group_by(generatorType,blocksMaxSize,blocksMinSize,blocksNumber,
 #g+geom_point()
 
 sstres = stres %>% group_by(generatorType,time,indic)%>% summarize(sdValue=sd(value),value=mean(value))
+
+g=ggplot(sstres[sstres$indic=='zombified'&sstres$generatorType!='random',],aes(x=time,y=value,ymin=value-sdValue,ymax=value+sdValue,colour=generatorType))
+g+geom_line()+geom_point()+geom_errorbar()+
+  ylab('Zombified')+xlab('Time')+stdtheme
+ggsave(paste0(resdir,'/spatialsens_generators_zombified.png'),width=25,height=15,units='cm')
+
+
 
 g=ggplot(sstres[sstres$indic=='humans'&sstres$generatorType!='random',],aes(x=time,y=value,ymin=value-sdValue,ymax=value+sdValue,colour=generatorType))
 g+geom_line()+geom_point()+geom_errorbar()+
--- openmole/spatialsens/calibration.R
+++ openmole/spatialsens/calibration.R
@@ -5,21 +5,53 @@ setwd(paste0(Sys.getenv('CS_HOME'),'/OpenMole/zombies/openmole/spatialsens'))
 
 source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
 
-resprefix = '20190418_153943_CALIB_GENERATORS_GRID/'
+#resprefix = '20190418_153943_CALIB_GENERATORS_GRID/'
+resprefix = '20190625_CALIB_GENERATORS_GRID/'
 resdir = paste0('calibration/',resprefix)
 figdir = paste0('results/',resprefix);dir.create(figdir)
 
 latestgen <- function(dir){max(as.integer(sapply(strsplit(sapply(strsplit(list.files(dir,pattern=".csv"),"population"),function(s){s[2]}),".csv"),function(s){s[1]})))}
 
-front <- as.tbl(read.csv(paste0(resdir,'population',latestgen(resdir),'.csv')))
+#front <- as.tbl(read.csv(paste0(resdir,'population',latestgen(resdir),'.csv')))
+#summary(front$evolution.samples)
 
-summary(front$evolution.samples)
-
-g=ggplot(front[front$evolution.samples>5,],aes(x=finalZombies,y=oppFinalHumans,col=generatorType,size=evolution.samples))
-g+geom_point(alpha=0.5)+xlab('Final zombies')+ylab('Opposite of final humans')+stdtheme
-ggsave(paste0(figdir,'pareto_samplesgt5.png'),width=20,height=18,units='cm')
+#g=ggplot(front[front$evolution.samples>5,],aes(x=finalZombies,y=oppFinalHumans,col=generatorType,size=evolution.samples))
+#g+geom_point(alpha=0.5)+xlab('Final zombies')+ylab('Opposite of final humans')+stdtheme
+#ggsave(paste0(figdir,'pareto_samplesgt5.png'),width=20,height=18,units='cm')
 
 #g=ggplot(front[abs(front$oppFinalHumans)>50&front$evolution.samples>20,],aes(x=finalZombies,y=oppFinalHumans,col=generatorType,size=evolution.samples))
 #g+geom_point(alpha=0.5)
+
+
+########
+## separately run calibrations
+
+frontExpMixture = as.tbl(read.csv(paste0('calibration/20190625_164608_CALIB_GENERATORS_GRID/expMixture/population',latestgen('calibration/20190625_164608_CALIB_GENERATORS_GRID/expMixture'),'.csv')))
+frontBlocks = as.tbl(read.csv(paste0('calibration/20190625_122904_CALIB_GENERATORS_GRID/blocks/population',latestgen('calibration/20190625_122904_CALIB_GENERATORS_GRID/blocks'),'.csv')))
+frontsPercolation = as.tbl(read.csv(paste0('calibration/20190625_163647_CALIB_GENERATORS_GRID/percolation/population',latestgen('calibration/20190625_163647_CALIB_GENERATORS_GRID/percolation'),'.csv')))
+
+allfronts = rbind(cbind(frontExpMixture,generator=rep('expMixture',nrow(frontExpMixture))),
+                  cbind(frontBlocks,generator=rep('blocks',nrow(frontBlocks))),
+                  cbind(frontsPercolation,generator=rep('percolation',nrow(frontsPercolation)))
+                  )
+
+
+g=ggplot(allfronts,aes(finalZombies,oppPeakSize,color=generator,size=evolution.samples))
+g+geom_point(alpha=0.5)+xlab("Final number of zombies")+ylab("Opposite of peak size")+
+  scale_size_continuous(name="samples")+stdtheme
+ggsave(file=paste0(figdir,'paretos_allsamples.png'),width=25,height=22,units='cm')
+
+g=ggplot(allfronts[allfronts$evolution.samples>=10,],aes(finalZombies,oppPeakSize,color=generator,size=evolution.samples))
+g+geom_point(alpha=0.5)+xlab("Final number of zombies")+ylab("Opposite of peak size")+
+  scale_size_continuous(name="samples")+stdtheme
+ggsave(file=paste0(figdir,'paretos_10samples.png'),width=25,height=22,units='cm')
+
+g=ggplot(allfronts[allfronts$evolution.samples>=20,],aes(finalZombies,oppPeakSize,color=generator,size=evolution.samples))
+g+geom_point(alpha=0.5)+xlab("Final number of zombies")+ylab("Opposite of peak size")+
+  scale_size_continuous(name="samples")+stdtheme
+ggsave(file=paste0(figdir,'paretos_20samples.png'),width=25,height=22,units='cm')
+
+
+
 
 
